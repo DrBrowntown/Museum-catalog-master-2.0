8ee61834091b63c87fa4b527578db1fd0525f723
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-03-06 15:57:21 -0500

    bug: Add correct path for Facebook secret json location

--- catalog/project.py
+++ catalog/project.py
@@ -68,10 +68,10 @@ def fbconnect():
     print "access token received %s " % access_token
 
 
-    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
+    app_id = json.loads(open('/var/www/museum-catalog/catalog/fb_client_secrets.json', 'r').read())[
         'web']['app_id']
     app_secret = json.loads(
-        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+        open('/var/www/museum-catalog/catalog/fb_client_secrets.json', 'r').read())['web']['app_secret']
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
         app_id, app_secret, access_token)
     h = httplib2.Http()
